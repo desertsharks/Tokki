ebb56e64f7fc43d08202b2aa485e6029e8554cf5
Ben Pevsner <pevsner.ben@gmail.com>
2015-04-09 20:02:09 -0700

    [add] Adds skeleton for front-end and back-end  - added views, controllers, services to front-end  - added server to back-end

--- server/app.js
+++ server/app.js
@@ -5,15 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
-var routes = require('./routes/index');
-var guests = require('./routes/guests');
-var host = require('./routes/host');
-
 var app = express();
+//define routers
+var guests = express.Router();
+var host = express.Router();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -21,10 +17,24 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/guests/*', guests);
-app.use('/host/*', host);
+
+/* GET home page. */
+app.get('/', function(req, res, next) {
+  res.sendFile('/index.html');
+});
+
+
+guests.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+app.use('/guests', guests);
+app.use('/host', host);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,7 +44,6 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
