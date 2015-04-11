b0fad36e2d89ee60cae6f0bd08581c9c4dc9a2ff
Ben Pevsner <pevsner.ben@gmail.com>
2015-04-10 19:14:20 -0700

    [test] Updates Gruntfile with basic tests  - Added Mocha/Chai for unit tests  - Added jshint for linting  - Added notify and nodemon, but didn't implement them  - Set up watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,168 +1,37 @@
 module.exports = function(grunt) {
-  // load up all of the necessary grunt plugins
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // testing plugins
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  //utility plugins
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-express-server');
-  grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-nodemon');
 
-
-  // in what order should the files be concatenated
-  var clientIncludeOrder = require('./include.conf.js');
 
   // grunt setup
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
 
-    // create a task called clean, which
-    // deletes all files in the listed folders
-    clean: {
-      dist: 'dist/*',
-      results: 'results/*'
-    },
-
-    // copy necessary files to our dist folder
-    copy: {
-      // create a task for client files
-      client: {
-        // Copy everything but the to-be-concatenated todo JS files
-        src: [ 'client/**', '!client/scripts/todo/**' ],
-        dest: 'dist/'
-      },
-      // create a task for server files
-      server: {
-        src: [ 'server/**' ],
-        dest: 'dist/'
-      }
-    },
-
-    // what files should be linted
     jshint: {
-      gruntfile: 'Gruntfile.js',
-      client: clientIncludeOrder,
-      server: 'server/**/*.js',
-      options: {
-        globals: {
-          eqeqeq: true
-        }
+      files: ['public/**/*.js', 'server/**/*']
+    },
+
+    mochaTest: {
+      test: {
+        src: ['test/**/*.js']
       }
     },
 
-    // uglify the files
-    uglify: {
-      todo: {
-        files: {
-          'dist/client/scripts/todo.js': clientIncludeOrder
-        }
-      }
-    },
-
-    // concat all the js files
-    concat: {
-      todo: {
-        files: {
-          // concat all the todo js files into one file
-          'dist/client/scripts/todo.js': clientIncludeOrder
-        }
-      }
-    },
-
-    // configure the server
-    express: {
-      dev: {
-        options: {
-          script: 'dist/server/server.js'
-        }
-      }
-    },
-
-    // configure karma
-    karma: {
-      options: {
-        configFile: 'karma.conf.js',
-        reporters: ['progress', 'coverage']
-      },
-      // Watch configuration
-      watch: {
-        background: true,
-        reporters: ['progress']
-      },
-      // Single-run configuration for development
-      single: {
-        singleRun: true,
-      },
-      // Single-run configuration for CI
-      ci: {
-        singleRun: true,
-        coverageReporter: {
-          type: 'lcov',
-          dir: 'results/coverage/'
-        }
-      }
-    },
-
-    // configure casperjs
-    casperjs: {
-      options: {},
-      e2e: {
-        files: {
-          'results/casper': 'test/e2e/**/*.js'
-        }
-      }
-    },
-
-    // create a watch task for tracking
-    // any changes to the following files
     watch: {
-      gruntfile: {
-        files: 'Gruntfile.js',
-        tasks: 'jshint:gruntfile'
-      },
-      client: {
-        files: [ 'client/**' ],
-        tasks: [ 'build', 'karma:watch:run', 'casperjs' ]
-      },
-      server: {
-        files: [ 'server/**' ],
-        tasks: [ 'build', 'express:dev', 'casperjs' ],
-        options: {
-          spawn: false // Restart server
-        }
-      },
-      unitTests: {
-        files: [ 'test/unit/**/*.js' ],
-        tasks: [ 'karma:watch:run' ]
-      },
-      integrationTests: {
-        files: [ 'test/integration/**/*.js' ],
-        tasks: [ 'karma:watch:run' ]
-      },
-      e2eTests: {
-        files: [ 'test/e2e/**/*.js' ],
-        tasks: [ 'casperjs' ]
+      jshint: {
+        files: ['public/**/*.js', 'server/**/*'],
+        tasks: ['jshint', 'mochaTest']
       }
     }
   });
 
-  // Perform a build
-  grunt.registerTask('build', [ 'jshint', 'clean', 'copy', 'concat', 'uglify']);
-
-  // Run e2e tests once
-  grunt.registerTask('teste2e', [ 'express:dev', 'casperjs' ]);
-
-  // Run client tests once
-  grunt.registerTask('testClient', [ 'karma:single' ]);
-
-  // Run all tests once
-  grunt.registerTask('test', [ 'testClient', 'teste2e']);
-
-  // Run all tests once
-  grunt.registerTask('ci', [ 'karma:ci', 'express:dev', 'casperjs' ]);
-
-  // Start watching and run tests when files change
-  grunt.registerTask('default', [ 'build', 'express:dev', 'karma:watch:start', 'watch' ]);
+  //'grunt watch' runs the watch function
+  grunt.registerTask('mocha', ['mochaTest']);
+  grunt.registerTask('hint', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
 };
--- test/server/SessionsCollectionSpec.js
+++ test/server/SessionsCollectionSpec.js
@@ -1,3 +1,4 @@
+var chai = require('chai');
 var assert = chai.assert;
 var should = chai.should();
 var expect = chai.expect;
