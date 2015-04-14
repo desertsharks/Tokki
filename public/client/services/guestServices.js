5d67b2451c78ac5ccb7a298b82c26c6e0cf0821f
Ben Pevsner <pevsner.ben@gmail.com>
2015-04-14 09:56:38 -0700

    [feature] Adds sockets to the front-end.  - Reorganizes the services to different files  - TODO: analysisServices.js

---
+++ public/client/services/guestServices.js
@@ -0,0 +1,51 @@
+// Socket helper functions
+app.factory('GuestServices', function($http) {
+
+  var session = {
+    id: "",
+    // TODO: Establish url
+    url: "localhost:4000/guest/",
+    socket: null
+  };
+
+  // Route to the guest view and listen on the new sessionId
+  var getSession = function(sessionId) {
+    session.id = sessionId;
+    return $http({
+      method: 'GET',
+      url: '/guest/' + sessionId
+    })
+    .then(function(resp) {
+      return resp.data;
+    });
+  };
+
+  // Initiates socket connection
+  // Listens for socket events
+  var listen = function() {
+    session.socket = io.connect(url + session.id);
+    session.socket.on('connect', function() {
+
+      // Listens for invalid room
+      session.socket.on('invalid', function(data) {
+        // TODO: Display a ui warning
+      });
+
+      // Listens for end of session
+      session.socket.on('end', function(data) {
+        // TODO: Activate end of session view
+      });
+    });
+  };
+
+  // Sends vote
+  var vote = function(voteData) {
+    session.socket.emit(voteData);
+  };
+
+  return {
+    getSession: getSession,
+    listen: listen
+  };
+
+});
---
+++ public/client/services/hostServices.js
@@ -0,0 +1,46 @@
+// Socket helper functions
+app.factory('HostServices', function($http) {
+
+  var session = {
+    id: "",
+    url: "localhost:4000/host/",
+    socket: null
+  };
+
+  // Sends a request for a new session.
+  // Receives the sessionID of that session.
+  var startSession = function() {
+    return $http({
+      method: 'POST',
+      url: session.url
+    })
+    .then(function(resp) {
+      session.id = resp.data;
+    });
+  };
+
+  // Initiates socket connection
+  // Listens for socket events
+  var listen = function() {
+    var socket = io.connect(session.url + session.id);
+    socket.on('connect', function() {
+      // Listens for stats
+      socket.on('stats', function(data) {
+        // TODO: Display Stats
+      });
+    });
+  };
+
+  // emit end to end a session
+  var vote = function() {
+    socket.emit('end');
+  };
+
+  return {
+    startSession: startSession,
+    listen: listen
+  };
+
+});
+
+
