af0e7ca240acbb8c89324cecb05a303aa20615d2
Thomas Logan <thomaslogan@Thomass-MacBook-Pro.local>
2015-04-14 14:17:13 -0700

    [feature] MVP routes are working. The page now loads the proper html chunks with the matching url.

--- public/client/app.js
+++ public/client/app.js
@@ -1,29 +1,30 @@
-angular.module('greenfield', ['ngRoute']);
+var app = angular.module('greenfield', ['ui.router']);
 
 //These are the routes that are necessary for the project MVP.
 
-angular.module('greenfield')
-  .config(function($routeProvider) {
-    $routeProvider
-    	//Routes to the home page 
-      .when('/home', {
-        templateUrl: './views/homeView.html',
-      })
-      //Routes to the guest session view
-      .when('/guest', {
-        templateUrl: './views/guestMainView.html',
-      })
-      //Routes to the host menu
-      .when('/hostMenu', {
-        templateUrl: './views/hostMenu.html',
-      })
-      //Routes to the hosts current session
-      .when('/hostSession', {
-        templateUrl: './views/hostMainView.html',
-      })
-      //If an unknown route is entered, it redirects to the home page.
-      .otherwise({
-        redirectTo: '/homeView.html'
-      });
-  });
+app.config(function($stateProvider, $urlRouterProvider) {
+  //If an unknown route is entered, it redirects to the home page.
+  $urlRouterProvider.otherwise('/home');
+  //Routes to the home page 
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: './views/homeView.html'
+    })
+    //Routes to the guest session view
+    .state('guestSession', {
+      url: '/guestSession',
+      templateUrl: './views/guestSession.html'
+    })
+    //Routes to the host menu
+    .state('hostMenu', {
+      url: '/hostMenu',
+      templateUrl: './views/hostMenu.html'
+    })
+    //Routes to the hosts current session
+    .state('hostSession', {
+      url: '/hostSession',
+      templateUrl: './views/hostSession.html'
+    });
+});
 
