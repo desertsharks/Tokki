5ad48f6707b7ced624b73f8469ede18e8b91049e
Ben Pevsner <pevsner.ben@gmail.com>
2015-04-17 17:42:13 -0700

    [feature] Stable version 1.0  - Fixes to displayed data  - Fixes guest client rerouting.  - Does NOT include data analysis  - Does NOT include auth  - Is NOT refactored.

--- public/client/controllers/hostController.js
+++ public/client/controllers/hostController.js
@@ -1,11 +1,16 @@
-angular.module('greenfield')
+angular.module('tokki')
   .controller('HostController', ['$scope', 'HostServices', function($scope, HostServices) {
 
   $scope.sessionId = 'no current session';
   $scope.currAvg = 0;
   $scope.hisAvg = 0;
   $scope.userCount = 0;
-  $scope.time = 'time';
+  $scope.time = moment().format('h:mm');
+  setInterval(function(){
+    $scope.time = moment().format('h:mm');
+    $scope.$apply();
+  }, 1000);
+
   // Opens a new session
   $scope.startSession = function() {
 
@@ -14,7 +19,6 @@ angular.module('greenfield')
       $scope.sessionId = newSessionId;
 
       HostServices.listen( function(sessionData) {
-        console.log(sessionData);
         $scope.userCount = sessionData.userCount || 0;
         $scope.currAvg = (sessionData.currentAverage || 0).toFixed(2);
         $scope.hisAvg = (sessionData.historicalAverage || 0).toFixed(2);
