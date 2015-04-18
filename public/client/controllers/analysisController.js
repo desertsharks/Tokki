ff471cd2e1195a938a19dafc96b61aeffa4fbf29
Janessa Link <janessa@gmail.com>
2015-04-18 13:02:08 -0700

    [refactor] TOKKI

--- public/client/controllers/analysisController.js
+++ public/client/controllers/analysisController.js
@@ -1,7 +1,16 @@
-angular.module('greenfield')
+angular.module('tokki')
   .controller('AnalysisController', ['$scope', 'AnalysisServices', function($scope, AnalysisServices) {
 
-  // Need to pull data from DB for analysis
+  $scope.selectedSessionId = null;
+  $scope.currHostId = null;
+
+  $scope.init = function(currHostId) {
+    AnalysisServices.sessionHistory( currHostId, function(sessionId, data) {
+      console.log('Populated data from host: ' + currHostId);
+    });
+  };
+
+  // Pulls data from DB for analysis
   $scope.sessionHistory = function(){
     console.log("Accessing session data");
     AnalysisServices.sessionHistory();
